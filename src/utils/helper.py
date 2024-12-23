8dacbe0c384fb601543fe2e079f2d37e16745e4e
shandytepe <shandy@pacmannai.com>
2024-12-23 17:35:17 +0700

    fix env

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,13 +1,25 @@
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_NAME_ANSWER = os.getenv("DB_NAME_ANSWER")
 
 
 def init_engine():
     cred = {
-        'host': 'ep-red-bar-a1rghkov-pooler.ap-southeast-1.aws.neon.tech',
-        'user': 'neondb_owner',
-        'pass': 'HmY1axWMVi8N',
-        'db': 'ecommerce_db',
-        'port': 5432
+        'host': DB_HOST,
+        'user': DB_USER,
+        'pass': DB_PASS,
+        'db': DB_NAME,
+        'port': DB_HOST
     }
 
     uri = f"postgresql://{cred['user']}:{cred['pass']}@{cred['host']}:{cred['port']}/{cred['db']}?sslmode=require"
@@ -19,11 +31,11 @@ def init_engine():
 
 def init_dest_engine():
     cred = {
-        'host': 'ep-red-bar-a1rghkov-pooler.ap-southeast-1.aws.neon.tech',
-        'user': 'neondb_owner',
-        'pass': 'HmY1axWMVi8N',
-        'db': 'answer_ecommerce_db',
-        'port': 5432
+        'host': DB_HOST,
+        'user': DB_USER,
+        'pass': DB_PASS,
+        'db': DB_NAME_ANSWER,
+        'port': DB_HOST
     }
 
     uri = f"postgresql://{cred['user']}:{cred['pass']}@{cred['host']}:{cred['port']}/{cred['db']}?sslmode=require"
